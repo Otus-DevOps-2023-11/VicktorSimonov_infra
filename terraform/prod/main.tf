60e3751e2cde55d2aa1ce87bde7bc85836fd9a15
VicktorSimonov <nikemsk@ya.ru>
2024-03-09 16:58:42 +0000

    terraform-2

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -13,16 +13,34 @@ provider "yandex" {
   zone      = var.zone
 }
 
-module "app" {
-  source          = "../modules/app"
-  public_key_path = var.public_key_path
-  app_disk_image  = var.app_disk_image
-  subnet_id       = var.subnet_id
-}
+#module "app" {
+#  source          = "../modules/app"
+#  public_key_path = var.public_key_path
+#  app_disk_image  = var.app_disk_image
+#  subnet_id       = var.subnet_id
+#}
 
+#module "db" {
+#  source          = "../modules/db"
+#  public_key_path = var.public_key_path
+#  db_disk_image   = var.db_disk_image
+#  subnet_id       = var.subnet_id
+#}
+
+module "app" {
+  source           = "../modules/app"
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
+  subnet_id        = module.vpc.subnet_id
+  name_prefix      = var.name_prefix
+}
 module "db" {
   source          = "../modules/db"
   public_key_path = var.public_key_path
-  db_disk_image   = var.db_disk_image
-  subnet_id       = var.subnet_id
+  subnet_id       = module.vpc.subnet_id
+  name_prefix     = var.name_prefix
+}
+module "vpc" {
+  source      = "../modules/vpc"
+  name_prefix = var.name_prefix
 }
