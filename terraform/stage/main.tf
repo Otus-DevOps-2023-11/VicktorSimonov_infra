64b44d2207612e8de90cf24aab772d1f9cfe54ed
VicktorSimonov <nikemsk@ya.ru>
2024-03-09 17:23:00 +0000

    terraform-2

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -29,16 +29,21 @@ provider "yandex" {
 
 module "app" {
   source           = "../modules/app"
-  public_key_path  = var.public_key_path
+  env              = var.env
+  depends_on       = [module.db]
   private_key_path = var.private_key_path
-  subnet_id        = module.vpc.subnet_id
-#  name_prefix      = var.name_prefix
+  public_key_path  = var.public_key_path
+  app_disk_image   = var.app_disk_image
+  subnet_id        = var.subnet_id
+  database_url     = module.db.internal_ip_address_db
 }
 module "db" {
   source           = "../modules/db"
+  env              = var.env
+  private_key_path = var.private_key_path
   public_key_path  = var.public_key_path
-  subnet_id        = module.vpc.subnet_id
-#  name_prefix      = var.name_prefix
+  db_disk_image    = var.db_disk_image
+  subnet_id        = var.subnet_id
 }
 module "vpc" {
   source = "../modules/vpc"
